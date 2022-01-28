51f0a8829af22757921b4791a8c3782a272732a1
Jake Tockerman <jaketockerman@gmail.com>
2022-01-28 13:56:20 -0500

    initial axios connection

--- backend/src/pybaseball_api.py
+++ backend/src/pybaseball_api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, json
 from pybaseball import standings
 
 app = Flask(__name__)
@@ -9,8 +9,9 @@ app = Flask(__name__)
 def get_standings(year=None):
     if year is not None:
         year = int(year)
-    return {"result": [standing.to_json() for standing in standings(year)]}
-
+    response = json.jsonify({"result": [standing.to_dict(orient="records") for standing in standings(year)]})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     app.run()
--- extension/src/components/Standings.tsx
+++ extension/src/components/Standings.tsx
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
+import PropTypes, { InferProps } from "prop-types";
 import logo from "./logo.svg";
 import "./Standings.css";
+import axios from "axios";
 
-function Standings() {
+Standings.propTypes = {
+	servers: PropTypes.shape({
+		pybaseball: PropTypes.string.isRequired,
+	}).isRequired,
+};
+
+interface Standings_Response {
+	result: Array<Division>;
+}
+
+interface Division {
+	teams: Array<Team>;
+}
+
+interface Team {
+	GB: string;
+	L: string;
+	Tm: string;
+	W: string;
+	"W-L%": string;
+}
+
+function Standings(props: InferProps<typeof Standings.propTypes>) {
+	// const [standings, setStandings] = useState();
+
+	useEffect(() => {
+		axios
+			.get<Standings_Response>(props.servers.pybaseball + "standings/")
+			.then((response) => {
+				console.log(response.data.result);
+			});
+	});
+
 	return (
 		<div className="App">
-			<img src={logo} className="App-logo" alt="logo" />
-			<p>
-				Edit <code>src/App.tsx</code> and save to reload.
-			</p>
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				This is the Standings page
-			</a>
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					This is the Standings page
+				</a>
+				{props.servers.pybaseball}
+				{/* {standings} */}
+			</header>
 		</div>
 	);
 }
