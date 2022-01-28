1c390fafba608011ceebdbbdd418cf6ce9adebf7
Jake Tockerman <jaketockerman@gmail.com>
2022-01-28 11:44:40 -0500

    usable standings endpoint

--- backend/src/pybaseball_api.py
+++ backend/src/pybaseball_api.py
@@ -4,8 +4,11 @@ from pybaseball import standings
 app = Flask(__name__)
 
 
-@app.route('/standings', methods=['GET'])
-def get_standings(year):
+@app.route('/standings/', methods=['GET'])
+@app.route('/standings/<year>', methods=['GET'])
+def get_standings(year=None):
+    if year is not None:
+        year = int(year)
     return {"result": [standing.to_json() for standing in standings(year)]}
 
 
