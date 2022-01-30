09bd4c29614d555caa356011cc6e4ef65ce6f89f
heintz0 <heintz0@purdue.edu>
2022-01-30 16:33:15 -0500

    Links Centered Properly

--- extension/src/components/NavBar.tsx
+++ extension/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Container, Nav, Navbar /*, Dropdown*/ } from "react-bootstrap";
+import { /*Container*/ Nav /* Navbar , Dropdown*/ } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 
@@ -12,39 +12,37 @@ class Navigation extends React.Component {
 	}
 
 	render() {
-		const myStyle = {
-			backgroundColor: "#00BDFF",
-			borderColor: "#1B1B1E",
-			color: "#000000",
-		};
 		return (
-			<Navbar variant="light">
-				<div className="Navigation">
-					<Nav>
-						<Container>
-							<Nav.Link as={Link} to="/" style={myStyle}>
-								Live
-							</Nav.Link>
-							<Nav.Link as={Link} to="/Standings" style={myStyle}>
-								Standings
-							</Nav.Link>
-						</Container>
-						{/*<Nav.Link as={Link} to = "/about" style={myStyle}>About</Nav.Link>*/}
-						{/* <Dropdown rendermenuonmount="true">
-                            <Dropdown.Toggle style={myStyle}
-                                    //    onMouseEnter = { this.handleOpen }
-                                    //    onMouseLeave = { this.handleClose }
-                                    //    open={ this.state.dropdownOpen }      
-                            >
-                                Projects
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                <Dropdown.Item as={Link} to="/projects/ece-362-mini-project" >ECE 362 Mini Project</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown> */}
-					</Nav>
+			<div className="topnav">
+				<div className="topnav-centered">
+					<Nav.Link as={Link} to="/" className="active">
+						Live
+					</Nav.Link>
 				</div>
-			</Navbar>
+
+				<Nav.Link as={Link} to="/">
+					Filler
+				</Nav.Link>
+
+				<div className="topnav-right">
+					<Nav.Link as={Link} to="/Standings">
+						Standings
+					</Nav.Link>
+				</div>
+			</div>
+			// <Navbar bg="dark" variant="dark">
+			// 	<Container>
+			// 		<Nav.Link as={Link} to="/" style={myStyle}>
+			//                Filler
+			// 		</Nav.Link>
+			// 		<Nav.Link as={Link} to="/" style={myStyle}>
+			// 				Live
+			// 		</Nav.Link>
+			// 		<Nav.Link as={Link} to="/Standings" style={myStyle}>
+			// 				Standings
+			// 		</Nav.Link>
+			// 	</Container>
+			// </Navbar>
 		);
 	}
 }
