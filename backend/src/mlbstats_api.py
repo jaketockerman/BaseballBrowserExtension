540c850967206cd87382096b15e950e8084bf6ad
Rufat Imanov <rimanov@purdue.edu>
2022-02-18 18:23:21 -0500

    deleted unnecessary files and updated mlbstats

--- backend/src/mlbstats_api.py
+++ backend/src/mlbstats_api.py
@@ -5,19 +5,29 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+# def get_game_date(game_id):
+#     return statsapi.schedule(game_id=game_id)
+
+
 @app.route('/standings/', methods=['GET'])
 @app.route('/standings/<year>', methods=['GET'])
 
-
 def get_standings(year=None):
-    # If year is not given, pull standings from last year
     if not year:
-        my_year = datetime.now().year - 1
+        if statsapi.standings(season=year):
+            my_year = datetime.now().year
+        else:
+            my_year = datetime.now().year - 1
     else:
         my_year = year
 
-    print("year:",my_year)
     return statsapi.standings(season=my_year)
+
+
+# @app.route('/schedule/<team>', methods=['GET'])
+
+# def get_schedule(team=None):
+    
 
 if __name__ == '__main__':
     app.run()
