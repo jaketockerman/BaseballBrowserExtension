aa443232685a9fa95981fb8ab8a608eec75ec47a
Jake Tockerman <jaketockerman@gmail.com>
2022-03-04 12:11:49 -0500

    working season-split data for player from fangraphs

--- backend/src/pybaseball_api.py
+++ backend/src/pybaseball_api.py
@@ -1,18 +1,17 @@
+import datetime
 from flask import Flask, json
-from pybaseball import standings
+from pybaseball import get_splits, playerid_reverse_lookup, batting_stats
 
 app = Flask(__name__)
 
-
-@app.route('/standings/', methods=['GET'])
-@app.route('/standings/<year>', methods=['GET'])
-def get_standings(year=None):
-    if year is not None:
-        year = int(year)
-    response = json.jsonify({"result": [standing.to_dict(orient="records") for standing in standings(year)]})
+@app.route('/player/<mlbamID>', methods=['GET'])
+def get_player(mlbamID: str):
+    #{"name_last":{"0":"yelich"},"name_first":{"0":"christian"},"key_mlbam":{"0":592885},"key_retro":{"0":"yelic001"},"key_bbref":{"0":"yelicch01"},"key_fangraphs":{"0":11477},"mlb_played_first":{"0":2013.0},"mlb_played_last":{"0":2021.0}}
+    #{"name_last":{"0":"braun"},"name_first":{"0":"ryan"},"key_mlbam":{"0":460075},"key_retro":{"0":"braur002"},"key_bbref":{"0":"braunry02"},"key_fangraphs":{"0":3410},"mlb_played_first":{"0":2007.0},"mlb_played_last":{"0":2020.0}}
+    player_ids = playerid_reverse_lookup([int(mlbamID)], key_type="mlbam").to_dict()
+    response = json.jsonify({"result": batting_stats(start_season=1871, end_season=datetime.now().year, players=player_ids["key_fangraphs"][0]).to_json(orient="records")})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-
 
 if __name__ == '__main__':
     app.run()
