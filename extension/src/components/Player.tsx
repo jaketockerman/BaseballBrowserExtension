6fa56231dfc23da073f9d00644bdac76c080867d
Jake Tockerman <jaketockerman@gmail.com>
2022-03-06 23:54:47 -0500

    initial dividing for player ui

--- extension/src/components/Player.tsx
+++ extension/src/components/Player.tsx
@@ -9,8 +9,31 @@ Player.propTypes = {
 	servers: PropTypes.object.isRequired as never as ServersType,
 };
 
+interface FGBattingStatsType {
+	AVG: number;
+}
+
+interface FGPitchingStatsType {
+	ERA: number;
+}
+
+interface PlayerStatsType {
+	batting: Array<FGBattingStatsType>;
+	pitching: Array<FGPitchingStatsType>;
+}
+
+interface FGResultType {
+	batting?: string;
+	pitching?: string;
+}
+
+interface FGResponseType {
+	result: FGResultType;
+}
+
 function Player(props: InferProps<typeof Player.propTypes>) {
-	const [playerDelay, setPlayerDelay] = useState<number | null>(1000);
+	const [playerDelay, setPlayerDelay] = useState<number | null>(500);
+	const [fgStats, setFGStats] = useState<PlayerStatsType>();
 	//test player 460075: Braun
 	//test player 592885: Yelich
 	//test player 660271: Ohtani (has both pitching and batting)
@@ -19,23 +42,19 @@ function Player(props: InferProps<typeof Player.propTypes>) {
 
 	useInterval(() => {
 		axios
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			.get<any>(props.servers.pybaseball + "player/" + props.mlbamID)
+			.get<FGResponseType>(
+				props.servers.pybaseball + "player/" + props.mlbamID
+			)
 			.then((response) => {
 				// setPlayerData(response.data.result);
 				setPlayerDelay(null);
-				console.log("batting");
-				console.log(
-					response.data.result.batting
-						? JSON.parse(response.data.result.batting)
-						: "no batting data"
-				);
-				console.log("pitching");
-				console.log(
-					response.data.result.pitching
-						? JSON.parse(response.data.result.pitching)
-						: "no pitching data"
-				);
+				const batting = response.data.result.batting
+					? JSON.parse(response.data.result.batting)
+					: [];
+				const pitching = response.data.result.pitching
+					? JSON.parse(response.data.result.pitching)
+					: [];
+				setFGStats({ batting: batting, pitching: pitching });
 			})
 			.catch((error: AxiosError<{ additionalInfo: string }>) => {
 				if (error.response?.status != 200) {
@@ -44,10 +63,39 @@ function Player(props: InferProps<typeof Player.propTypes>) {
 			});
 	}, playerDelay);
 
+	function player_info() {
+		const style = {
+			borderBottom: "1px solid",
+			borderColor: "#FFFFFF",
+		};
+
+		return (
+			<div className="tw-h-3/6 tw-w-full" style={style}>
+				{props.mlbamID}
+			</div>
+		);
+	}
+
+	function display_player() {
+		console.log("batting");
+		console.log(fgStats?.batting ? fgStats?.batting : "no batting data");
+		console.log("pitching");
+		console.log(fgStats?.pitching ? fgStats?.pitching : "no pitching data");
+		return (
+			<React.Fragment>
+				<React.Fragment>{player_info()}</React.Fragment>
+				<div className="tw-h-full tw-w-full">test_under</div>
+			</React.Fragment>
+		);
+	}
+
 	return (
 		<div className="tw-flex tw-flex-col tw-h-full tw-items-center tw-justify-center">
-			<div>This is the Player page {props.mlbamID} </div>
-			<div></div>
+			{fgStats != undefined ? (
+				display_player()
+			) : (
+				<React.Fragment> Loading... </React.Fragment>
+			)}
 		</div>
 	);
 }
